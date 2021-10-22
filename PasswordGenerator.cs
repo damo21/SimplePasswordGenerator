f82add94599be23d47f55516ab2530884a95874f
Damian Klekot <damianklekot123@gmail.com>
2021-10-22 19:48:31 +0100

    Final

---
+++ PasswordGenerator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace PasswordGenerator
+{
+    class PasswordGenerator
+    {
+        Random random = new Random();
+
+        List<string> upperAlphabet = new List<string>()
+           {
+            "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+            };
+
+        List<string> lowerAlphabet = new List<string>()
+          {
+            "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
+          };
+
+        List<int> numbers = new List<int>()
+          {
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+          };
+
+        List<string> symbols = new List<string>()
+        {
+            "!", "£", "$", "%", "&", "/", "^", "*"
+        };
+
+        List<string> password = new List<string>();
+
+        string uLetter, lLetter, num, sym, pass = "";
+
+        public void genPass()
+        {
+            while (password.Count < 3)
+            {
+                int index = random.Next(upperAlphabet.Count);
+                uLetter = upperAlphabet[index];
+                password.Add(uLetter);
+
+                while (password.Count >= 3 && password.Count < 6)
+                {
+                    index = random.Next(lowerAlphabet.Count);
+                    lLetter = lowerAlphabet[index];
+                    password.Add(lLetter);
+
+
+                    while (password.Count >= 6 && password.Count < 9)
+                    {
+                        index = random.Next(numbers.Count);
+                        num = numbers[index].ToString();
+                        password.Add(num);
+
+                        while (password.Count >= 9 && password.Count < 12)
+                        {
+                            index = random.Next(symbols.Count);
+                            sym = symbols[index];
+                            password.Add(sym);
+                        }
+                    }
+                }
+            }
+            var shuffled = password.OrderBy(random => Guid.NewGuid()).ToList();
+
+            Console.WriteLine(string.Join("", shuffled));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PasswordGenerator
+{
+
+    class Program
+    {
+            static void Main(string[] args)
+        {
+            PasswordGenerator p = new PasswordGenerator();
+            Console.WriteLine("Welcome to  Damians 12Char Password Generator!");
+
+            int num;
+            string userChoice;
+
+            do
+            {
+               
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("Select the number of your choice: ");
+                Console.WriteLine("1: Generate Password");
+                Console.WriteLine("2: Quit");
+                Console.WriteLine("----------------------------------");
+
+                userChoice = Console.ReadLine();
+
+                if (!Int32.TryParse(userChoice, out num)) continue;
+
+                if (userChoice == "2")
+                {
+                    Environment.Exit(0);
+                }
+
+                if (userChoice == "1")
+                {
+                    Console.WriteLine("----------------------------------");
+                    p.genPass();
+                }
+                else
+                {
+                    Console.WriteLine("Well done you found the Easter Egg!");
+                    Environment.Exit(0);
+                }
+
+            } while (true);
+        }
+    }
+}
+
+
